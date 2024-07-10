dc9549d110ce7e46065b4ae1fcac9429c35e57f2
loleo <pattrapol.nope@gmail.com>
2024-07-10 22:09:56 +0700

    week2

--- physical/Lab 02-08 3Person.c
+++ physical/Lab 02-08 3Person.c
@@ -3,13 +3,15 @@
 int main(){
     char fname1[30];
     char sname1[30];
-    char person2[30];
-    char person3[30];
-    scanf(" %[^\n]", fname1);
-    scanf(" %[^\n]", sname1);
-    scanf(" %[^\n]", person2);
-    scanf(" %[^\n]", person3);
+    char fperson1[30];
+    char sperson1[30];
+    char fperson2[30];
+    char sperson2[30];
+    scanf("%s", fname1);
+    scanf("%s", sname1);
+    scanf("%s %s", fperson1, sperson1);
+    scanf("%s %s", fperson2, sperson2);
     printf("Person 1: %s %s\n", fname1, sname1);
-    printf("Person 2: %s\n", person2);
-    printf("Person 3: %s\n", person3);
+    printf("Person 2: %s %s\n", fperson1, sperson1);
+    printf("Person 3: %s %s\n", fperson2, sperson2);
 }
