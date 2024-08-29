e9fa8d409464375b7643cd8d022270f67e55f8f9
Loleo✨ <105137584+Pyaowara@users.noreply.github.com>
2024-08-29 23:06:09 +0700

    nah

--- physical/Lab_06-06_Sort_name_normal.c
+++ physical/Lab_06-06_Sort_name_normal.c
@@ -18,12 +18,12 @@ void totile(char str[]){
     }
 }
 
-void bubbleSort(char arr[][60], int n) {
+void bubbleSort(char arr[][60]) {
     char temp[60];
     int swapped;
     
-    for (int i = 0; i < n-1; i++) {
-        for (int j = 0; j < n-i-1; j++) {
+    for (int i = 0; i < 20-1; i++) {
+        for (int j = 0; j < 20-i-1; j++) {
             if (strcmp(arr[j], arr[j+1]) > 0) {
                 strcpy(temp, arr[j]);
                 strcpy(arr[j], arr[j+1]);
@@ -37,16 +37,15 @@ void bubbleSort(char arr[][60], int n) {
 }
 
 int main(){
+    char name[20][60];
     char temp[60];
-    int n;
-    scanf("%d", &n);
-    char name[n][60];
-
-    for (int i = 0; i < n; i++){
+    scanf("%[^\n]s", name[0]);
+    totile(name[0]);
+    for (int i = 1; i < 20; i++){
         scanf("\n%[^\n]s", name[i]);
         totile(name[i]);
     }
-    bubbleSort(name, n);
-    for (int i = 0; i < n; i++)
+    bubbleSort(name);
+    for (int i = 0; i < 20; i++)
         printf("%s\n", name[i]);
 }
